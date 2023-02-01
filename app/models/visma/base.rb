a98eab6496a407c45cb494fe49b6271531ec6ed5
Viktor Basso <viktor@kuraas.no>
2023-02-01 11:34:46 +0100

    ??

--- app/models/visma/base.rb
+++ app/models/visma/base.rb
@@ -2,6 +2,6 @@ module Visma
   # Common methods for Visma models
   class Base < ActiveRecord::Base
     establish_connection :visma
-    self.table_name_prefix = VISMA_CONFIG['table_name_prefix']
+    self.table_name = VISMA_CONFIG['table_name_prefix']
   end
 end
